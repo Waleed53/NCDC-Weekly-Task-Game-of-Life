f9e13f7f00fc3ff7fddaee3d4a5117a114099b22
Waleed53 <127500143+Waleed53@users.noreply.github.com>
2026-04-18 20:49:36 +0500

    Add Game of Life: WeeklyTask/Weekly Task Code/lab1b.c

---
+++ WeeklyTask/Weekly Task Code/lab1b.c
@@ -0,0 +1,122 @@
+/*
+ * lab1b.c
+ *
+ *  Created on:
+ *      Author:
+ */
+
+/* include helper functions for game */
+#include "lifegame.h"
+#include "stdio.h"
+/* add whatever other includes here */
+
+/* number of generations to evolve the world */
+#define NUM_GENERATIONS 50
+
+/* functions to implement -- can copy from Part A */
+
+/* this function should set the state of all
+   the cells in the next generation and call
+   finalize_evolution() to update the current
+   state of the world to the next generation */
+void next_generation(void);
+
+/* this function should return the state of the cell
+   at (x,y) in the next generation, according to the
+   rules of Conway's Game of Life (see handout) */
+int get_next_state(int x, int y);
+
+/* this function should calculate the number of alive
+   neighbors of the cell at (x,y) */
+int num_neighbors(int x, int y);
+
+int main(int argc, char ** argv)
+{
+	int n;
+
+	/* TODO: initialize the world, from file argv[1]
+	   if command line argument provided (argc > 1), or
+	   using hard-coded pattern (use Part A) otherwise */
+	if(argc > 1){
+	
+	initialize_world_from_file(argv[1]);
+	}else{
+	initialize_world();
+	}
+
+	for (n = 0; n < NUM_GENERATIONS; n++)
+		next_generation();
+
+	/* TODO: output final world state to console and save
+	   world to file "world.txt" in current directory */
+	output_world();
+	save_world_to_file("world.txt");
+
+	return 0;
+}
+
+void next_generation(void) {
+	/* TODO: for every cell, set the state in the next
+	   generation according to the Game of Life rules
+
+	   Hint: use get_next_state(x,y) */
+	int x,y;
+	int w = get_world_width();
+	int h = get_world_height();
+	
+	for(x = 0; x<w; x++){
+		for(y=0; y<h; y++){
+			int next_state = get_next_state(x,y);
+			set_cell_state(x,y,next_state);
+		}
+	}
+
+	finalize_evolution(); /* called at end to finalize */
+}
+
+int get_next_state(int x, int y) {
+	/* TODO: for the specified cell, compute the state in
+	   the next generation using the rules
+
+	   Use num_neighbors(x,y) to compute the number of live
+	   neighbors */
+	int current_state = get_cell_state(x,y);
+	int neighbors = num_neighbors(x,y);
+	if(current_state == ALIVE){
+		if(neighbors < 2){
+			return DEAD;
+		} else if(neighbors ==2 | neighbors ==3){
+			return ALIVE;
+		}else {
+			return DEAD;
+		}
+	} else {
+		if(neighbors == 3) {
+			return ALIVE;
+		} else {
+			return DEAD;
+		}
+	}
+}
+
+int num_neighbors(int x, int y) {
+	/* TODO: for the specified cell, return the number of
+	   neighbors that are ALIVE
+
+	   Use get_cell_state(x,y) */
+	int count = 0;
+	int dx, dy;
+	
+	for(dx = -1; dx <=1; dx++) {
+		for(dy=-1; dy<=1; dy++){
+			if(dx==0 && dy==0){
+				continue;
+			}
+			if(get_cell_state(x+dx, y+dy) == ALIVE){
+				count++;
+			}
+		}
+	}
+	
+	return count;
+}
